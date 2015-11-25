916ae290d99bc1b9a91fe7f0e260dfe79c6d1d77
Ryan Yep <ryan.yep@gmail.com>
2015-11-24 20:40:24 -0600

    Create todos table prior to migration

--- db/migrate/20151125022616_create_todos.rb
+++ db/migrate/20151125022616_create_todos.rb
@@ -1,7 +1,10 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-
+      t.string   :task
+      t.text     :note
+      t.datetime :due_date
+      t.boolean  :completed, default: false
       t.timestamps null: false
     end
   end
